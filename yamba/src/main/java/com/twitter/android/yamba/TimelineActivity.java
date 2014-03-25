05067643fe497d135c950023b7a199461fe4069a
Ken Jones <kennethj@twitter.com>
2014-03-25 14:25:44 +0000

    Implemented a basic query/SimpleCursorAdapter model for TimelineActivity.

--- yamba/src/main/java/com/twitter/android/yamba/TimelineActivity.java
+++ yamba/src/main/java/com/twitter/android/yamba/TimelineActivity.java
@@ -5,11 +5,21 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.twitter.university.android.yamba.service.YambaContract;
 
 
 public class TimelineActivity extends Activity {
+    private static final String[] FROM = {
+      YambaContract.Timeline.Columns.HANDLE,
+      YambaContract.Timeline.Columns.TWEET,
+    };
+    private static final int[] TO = {
+      android.R.id.text1,
+      android.R.id.text2,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,16 @@ public class TimelineActivity extends Activity {
                 null,
                 null,
                 YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
-        startManagingCursor(cursor);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                cursor,
+                FROM,
+                TO,
+                0
+        );
+        ListView list = (ListView) findViewById(R.id.list_timeline);
+        list.setAdapter(adapter);
     }
 
 

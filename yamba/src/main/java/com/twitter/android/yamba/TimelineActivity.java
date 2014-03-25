b2f4fdff07989ee490d3484d0f0e682061e33611
Ken Jones <kennethj@twitter.com>
2014-03-25 14:46:55 +0000

    Redesigned TimelineActivity to use a Loader.

--- yamba/src/main/java/com/twitter/android/yamba/TimelineActivity.java
+++ yamba/src/main/java/com/twitter/android/yamba/TimelineActivity.java
@@ -1,6 +1,9 @@
 package com.twitter.android.yamba;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,7 +14,8 @@ import android.widget.SimpleCursorAdapter;
 import com.twitter.university.android.yamba.service.YambaContract;
 
 
-public class TimelineActivity extends Activity {
+public class TimelineActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int TIMELINE_LOADER = 42;
     private static final String[] FROM = {
       YambaContract.Timeline.Columns.HANDLE,
       YambaContract.Timeline.Columns.TWEET,
@@ -20,34 +24,50 @@ public class TimelineActivity extends Activity {
       android.R.id.text1,
       android.R.id.text2,
     };
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        Cursor cursor = getContentResolver().query(
-                YambaContract.Timeline.URI,
-                null,
-                null,
-                null,
-                YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+        mAdapter = new SimpleCursorAdapter(
                 this,
                 android.R.layout.simple_list_item_2,
-                cursor,
+                null,
                 FROM,
                 TO,
                 0
         );
         ListView list = (ListView) findViewById(R.id.list_timeline);
-        list.setAdapter(adapter);
+        list.setAdapter(mAdapter);
+        getLoaderManager().initLoader(TIMELINE_LOADER, null, this);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getApplicationContext(),
+                YambaContract.Timeline.URI,
+                null,
+                null,
+                null,
+                YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.timeline, menu);
         return true;
